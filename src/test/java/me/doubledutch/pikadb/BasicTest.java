878541d6984ea4aeebda0438693a0b98b8cd557c
Kasper Jeppe Jeppesen <kasper.jeppesen@gmail.com>
2015-08-30 12:28:06 -0700

    Setup Build

---
+++ src/test/java/me/doubledutch/pikadb/BasicTest.java
@@ -0,0 +1,11 @@
+package me.doubledutch.pikadb;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class BasicTest{
+	@Test
+	public void testAllTheThings(){
+		assertTrue(true);
+	}
+}
