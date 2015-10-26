e8b9ec36e9dce3b8de6ca08f259c73e90dc0a30d
Kasper Jeppesen <kasper@doubledutch.me>
2015-10-26 10:46:13 -0700

    Add pikadb

---
+++ src/main/java/me/doubledutch/pikadb/PikaDB.java
@@ -0,0 +1,14 @@
+package me.doubledutch.pikadb;
+
+import java.io.*;
+
+public class PikaDB{
+	public PikaDB(String fileName) throws IOException{
+
+	}
+	public PikaDB(File file) throws IOException{
+		
+	}
+
+
+}
